105b0cb3f603c00f7a652268bab231325aa01669
erickcelio <erickcelio@hotmail.com>
2019-03-09 14:30:29 -0300

    exercicio1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import exercicio1.Aluno;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/exercicio1/Aluno.java
@@ -0,0 +1,54 @@
+package exercicio1;
+
+import java.util.ArrayList;
+
+public class Aluno {
+
+    public String nome;
+    public double notas[];
+    public double media;
+    private String sala;
+
+    public Aluno(String nome, double[] notas, String sala) {
+        this.nome = nome;
+        this.sala = sala;
+        this.notas = notas;
+        double soma = 0;
+        for (int i = 0; i < notas.length; i++) {
+            soma = soma + notas[i];
+        }
+        this.media = soma/notas.length;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public String getSala() {
+        return sala;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
+}
